d93f1606c47ef58b48685e79f5b982b8ae9a6421
cody <codyd@qq.com>
2019-10-15 22:36:08 +0800

    remove flag '--fast-list'

--- rclone_sa_magic.py
+++ rclone_sa_magic.py
@@ -169,7 +169,7 @@ def main():
             open_cmd += "--dry-run "
 
         open_cmd += "--drive-server-side-across-configs --rc -vv --ignore-existing " \
-                    "--tpslimit 3 --transfers 3 --drive-chunk-size 32M --fast-list " \
+                    "--tpslimit 3 --transfers 3 --drive-chunk-size 32M " \
                     "--drive-acknowledge-abuse --log-file={} {} {}".format(logfile,
                                                 src_full_path,
                                                 dst_full_path)
