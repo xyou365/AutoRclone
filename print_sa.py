0f2757a3e98bf496048e0e0a074b6480c7a8ff97
steven zhu <va3-az-git@stevenz.net>
2020-07-21 23:10:17 -0400

    add line seperator & preventive fix

--- print_sa.py
+++ print_sa.py
@@ -15,10 +15,17 @@ parse = argparse.ArgumentParser(
                 'files.')
 parse.add_argument('--path', '-p', default='accounts',
                    help='Specify an alternative path to the service accounts folder.')
+parse.add_argument('--sa', '-sa', default='salist.txt',
+                   help='Specify an alternative path to generate list of your service accounts.')
+parse.add_argument('--seperator', '-sp', default='0',
+                   help='Insert a empty line for every x lines to better seperate email adding process.')
+
 # service-account@googlegroups.com
 
 args = parse.parse_args()
 acc_dir = args.path
+sa_file = args.sa
+sp_num = args.seperator
 
 sa = glob.glob('%s/*.json' % acc_dir)
 
@@ -38,6 +45,8 @@ hours, rem = divmod((time.time() - stt), 3600)
 minutes, sec = divmod(rem, 60)
 print("Elapsed Time:\n{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), sec))
 
-with open('salist.txt', 'w') as f:
-    for i in saList1:
-        print(i, file=f)
+with open(sa_file, 'w') as f:
+    for i in range(len(saList1)):
+        if ((int(sp_num) <= len(saList1)+1) and (int(sp_num) != 0) and (i % int(sp_num) == 0)):
+           print("",  file=f)
+        print(saList1[i], file=f)
