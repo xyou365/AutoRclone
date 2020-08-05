589bfd23054a3034c82e1b8d35dc3dcbede8be3f
Steven Zhu <github@stevenz.net>
2020-08-05 15:53:17 -0400

    Add color coding and show warning when duplicate found

--- print_sa.py
+++ print_sa.py
@@ -10,7 +10,36 @@ from __future__ import print_function
 import argparse
 import glob
 import json
+import sys
 import time
+
+
+# Colored printing functions for strings that use universal ANSI escape sequences.
+# fail: bold red, pass: bold green, warn: bold yellow,
+# info: bold blue, bold: bold white
+
+class ColorPrint:
+
+    @staticmethod
+    def print_fail(message, end='\n'):
+        sys.stderr.write('\x1b[1;31m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_pass(message, end='\n'):
+        sys.stdout.write('\x1b[1;32m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_warn(message, end='\n'):
+        sys.stderr.write('\x1b[1;33m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_info(message, end='\n'):
+        sys.stdout.write('\x1b[1;34m' + message.strip() + '\x1b[0m' + end)
+
+    @staticmethod
+    def print_bold(message, end='\n'):
+        sys.stdout.write('\x1b[1;37m' + message.strip() + '\x1b[0m' + end)
+
 
 stt = time.time()
 
@@ -36,26 +65,41 @@ sa = glob.glob('%s/*.json' % acc_dir)
 
 saList = []
 
-print('Generating List')
+ColorPrint.print_info('Generating List')
 
 for i in sa:
     ce = json.loads(open(i, 'r').read())['client_email']
     saList.append(ce)
 
-print('List generated')
-print('Deduping...')
+initSASize = len(saList)
+if (initSASize == 0):
+    raise ValueError('No Service Account json file found under %s directory' % acc_dir)
+else:
+    ColorPrint.print_info('List generated')
+
+ColorPrint.print_info('Deduping...')
 saList = list(dict.fromkeys(saList))
+deduSASize = len(saList)
+
 if (sa_project != ""):
-    print("Filtering email from %s." % sa_project)
+    ColorPrint.print_info("Filtering email from %s." % sa_project)
     saList = [n for n in saList if sa_project in n]
-print('Complete.')
+    if (len(saList) == 0):
+        raise ValueError('No Service Account json file matches %s' % sa_project)
 
-hours, rem = divmod((time.time() - stt), 3600)
-minutes, sec = divmod(rem, 60)
-print("Elapsed Time:\n{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), sec))
-
+print("Writing SA to file.")
 with open(sa_file, 'w') as f:
     for i in range(len(saList)):
         if ((int(sp_num) <= len(saList) + 1) and (int(sp_num) != 0) and (i % int(sp_num) == 0)):
             print("", file=f)
         print(saList[i], file=f)
+
+ColorPrint.print_pass('Complete.\n %i SA write to file' % len(saList))
+ColorPrint.print_info('Your file is located at %s' % sa_file)
+
+hours, rem = divmod((time.time() - stt), 3600)
+minutes, sec = divmod(rem, 60)
+ColorPrint.print_info("Elapsed Time: {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), sec))
+
+if (initSASize != deduSASize):
+    ColorPrint.print_warn("Your SA directory contains %i duplicate." % (initSASize - deduSASize))
